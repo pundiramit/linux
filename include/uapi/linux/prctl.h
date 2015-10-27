3e984971bc641a2e2c4c6777037757940d3920db
Colin Cross <ccross@android.com>
2015-10-27 16:42:08 -0700

    ANDROID: mm: add a field to store names for private anonymous memory
    Userspace processes often have multiple allocators that each do
    anonymous mmaps to get memory.  When examining memory usage of
    individual processes or systems as a whole, it is useful to be
    able to break down the various heaps that were allocated by
    each layer and examine their size, RSS, and physical memory
    usage.
    
    This patch adds a user pointer to the shared union in
    vm_area_struct that points to a null terminated string inside
    the user process containing a name for the vma.  vmas that
    point to the same address will be merged, but vmas that
    point to equivalent strings at different addresses will
    not be merged.
    
    Userspace can set the name for a region of memory by calling
    prctl(PR_SET_VMA, PR_SET_VMA_ANON_NAME, start, len, (unsigned long)name);
    Setting the name to NULL clears it.
    
    The names of named anonymous vmas are shown in /proc/pid/maps
    as [anon:<name>] and in /proc/pid/smaps in a new "Name" field
    that is only present for named vmas.  If the userspace pointer
    is no longer valid all or part of the name will be replaced
    with "<fault>".
    
    The idea to store a userspace pointer to reduce the complexity
    within mm (at the expense of the complexity of reading
    /proc/pid/mem) came from Dave Hansen.  This results in no
    runtime overhead in the mm subsystem other than comparing
    the anon_name pointers when considering vma merging.  The pointer
    is stored in a union with fieds that are only used on file-backed
    mappings, so it does not increase memory usage.
    
    Includes fix from Jed Davis <jld@mozilla.com> for typo in
    prctl_set_vma_anon_name, which could attempt to set the name
    across two vmas at the same time due to a typo, which might
    corrupt the vma list.  Fix it to use tmp instead of end to limit
    the name setting to a single vma at a time.
    
    Bug: 120441514
    Change-Id: I9aa7b6b5ef536cd780599ba4e2fba8ceebe8b59f
    Signed-off-by: Dmitry Shmidt <dimitrysh@google.com>
    [AmitP: Fix get_user_pages_remote() call to align with upstream commit
            5b56d49fc31d ("mm: add locked parameter to get_user_pages_remote()")]
    Signed-off-by: Amit Pundir <amit.pundir@linaro.org>
    Signed-off-by: Matthias Maennich <maennich@google.com>
    
    vma naming fixup for 5.8-rc1
    
    Change-Id: I41c1a5cf0674be1ff26b80938c78283cc32e05f9
    
    fixup vma naming patch for 5.9-rc
    
    Change-Id: I7c1e3d3519e6d941d4184d7ea77feb8d3a299adb
    Signed-off-by: John Stultz <john.stultz@linaro.org>
    
    vma-naming: 5.9-rc fixup
    
    Change-Id: I891099bdfb4bb62db0fa5c27ed5edae586e93f89

--- include/uapi/linux/prctl.h
+++ include/uapi/linux/prctl.h
@@ -254,5 +254,7 @@ struct prctl_mm_map {
 /* The control values for the user space selector when dispatch is enabled */
 # define SYSCALL_DISPATCH_FILTER_ALLOW	0
 # define SYSCALL_DISPATCH_FILTER_BLOCK	1
+#define PR_SET_VMA		0x53564d41
+# define PR_SET_VMA_ANON_NAME		0
 
 #endif /* _LINUX_PRCTL_H */
