d8f7591ad6d0d68d51831d06095e8db5dc96d929
Bjorn Andersson <bjorn.andersson@linaro.org>
2019-12-26 14:17:09 -0800

    iommu/arm-smmu: Allow inherting stream mapping from bootloader
    The Qualcomm bootloaders leaves the IOMMU with stream mapping for
    the display hardware to be able to read the framebuffer memory in DDR,
    to continuously display a boot splash or to implement EFI framebuffer.
    
    This patch implements support for implementations to pin stream mappings
    and adds the code to the Qualcomm implementation for reading out the
    stream mapping from the bootloader, with the result of maintaining the
    display hardware's access to DDR until the context bank is enabled.
    
    Heavily based on downstream implementation by Patrick Daly
    <pdaly@codeaurora.org>.
    
    Signed-off-by: Bjorn Andersson <bjorn.andersson@linaro.org>
    Link: https://lore.kernel.org/r/20191226221709.3844244-4-bjorn.andersson@linaro.org
    Signed-off-by: Bjorn Andersson <bjorn.andersson@linaro.org>
    [jstultz: Minor build fix for v5.7-rc1]
    Signed-off-by: John Stultz <john.stultz@linaro.org>
    Change-Id: I200d25fd2f05886a602e6dabaac339b0e3c35062

--- drivers/iommu/arm-smmu-qcom.c
+++ drivers/iommu/arm-smmu-qcom.c
@@ -4,6 +4,7 @@
  */
 
 #include <linux/of_device.h>
+#include <linux/bitfield.h>
 #include <linux/qcom_scm.h>
 
 #include "arm-smmu.h"
@@ -29,6 +30,39 @@ static int qcom_smmu_def_domain_type(struct device *dev)
 		of_match_device(qcom_smmu_client_of_match, dev);
 
 	return match ? IOMMU_DOMAIN_IDENTITY : 0;
+};
+
+static int qcom_sdm845_smmu500_cfg_probe(struct arm_smmu_device *smmu)
+{
+	u32 s2cr;
+	u32 smr;
+	int i;
+
+	for (i = 0; i < smmu->num_mapping_groups; i++) {
+		smr = arm_smmu_gr0_read(smmu, ARM_SMMU_GR0_SMR(i));
+		s2cr = arm_smmu_gr0_read(smmu, ARM_SMMU_GR0_S2CR(i));
+
+		smmu->smrs[i].mask = FIELD_GET(ARM_SMMU_SMR_MASK, smr);
+		smmu->smrs[i].id = FIELD_GET(ARM_SMMU_SMR_ID, smr);
+		if (smmu->features & ARM_SMMU_FEAT_EXIDS)
+			smmu->smrs[i].valid = FIELD_GET(ARM_SMMU_S2CR_EXIDVALID, s2cr);
+		else
+			smmu->smrs[i].valid = FIELD_GET(ARM_SMMU_SMR_VALID, smr);
+
+		smmu->s2crs[i].group = NULL;
+		smmu->s2crs[i].count = 0;
+		smmu->s2crs[i].type = FIELD_GET(ARM_SMMU_S2CR_TYPE, s2cr);
+		smmu->s2crs[i].privcfg = FIELD_GET(ARM_SMMU_S2CR_PRIVCFG, s2cr);
+		smmu->s2crs[i].cbndx = FIELD_GET(ARM_SMMU_S2CR_CBNDX, s2cr);
+
+		if (!smmu->smrs[i].valid)
+			continue;
+
+		smmu->s2crs[i].pinned = true;
+		bitmap_set(smmu->context_map, smmu->s2crs[i].cbndx, 1);
+	}
+
+	return 0;
 }
 
 static int qcom_sdm845_smmu500_reset(struct arm_smmu_device *smmu)
@@ -63,6 +97,7 @@ static int qcom_smmu500_reset(struct arm_smmu_device *smmu)
 static const struct arm_smmu_impl qcom_smmu_impl = {
 	.def_domain_type = qcom_smmu_def_domain_type,
 	.reset = qcom_smmu500_reset,
+	.cfg_probe = qcom_sdm845_smmu500_cfg_probe,
 };
 
 struct arm_smmu_device *qcom_smmu_impl_init(struct arm_smmu_device *smmu)
