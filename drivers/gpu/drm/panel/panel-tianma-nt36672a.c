58db711c5e7939cdbb30ea0a1bf6cd77978e3892
Amit Pundir <amit.pundir@linaro.org>
2020-01-30 20:20:47 +0530

    Revert "panel-tianma-nt36672a: Fix drm_panel_init() call"
    This reverts commit 566395e1249d2cef27cab4c95431adfe5b113f20.
    
    Change-Id: Ide9afc63b23f4dda1cb1d3446c99bb59058e3e4f

--- drivers/gpu/drm/panel/panel-tianma-nt36672a.c
+++ drivers/gpu/drm/panel/panel-tianma-nt36672a.c
@@ -883,9 +883,10 @@ pr_err("In nt36672a panel add\n");
 	if (IS_ERR(pinfo->backlight))
 		return PTR_ERR(pinfo->backlight);
 
-	drm_panel_init(&pinfo->base, dev, &panel_funcs,
-		       DRM_MODE_CONNECTOR_DSI);
+	drm_panel_init(&pinfo->base);
 pr_err("In nt36672a panel add: after drm_panel_init\n");
+	pinfo->base.funcs = &panel_funcs;
+	pinfo->base.dev = &pinfo->link->dev;
 
 	ret = drm_panel_add(&pinfo->base);
 	if (ret < 0)
