57ace204e6717fd2bfbe6950ac5e451a10285d32
Amit Pundir <amit.pundir@linaro.org>
2020-08-12 15:30:58 +0530

    FROMLIST: ath10k: qmi: Skip host capability request for Xiaomi Poco F1
    v2: https://lkml.org/lkml/2020/11/23/200
    
    Workaround to get WiFi working on Xiaomi Poco F1 (sdm845)
    phone. We get a non-fatal QMI_ERR_MALFORMED_MSG_V01 error
    message in ath10k_qmi_host_cap_send_sync(), but we can still
    bring up WiFi services successfully on AOSP if we ignore it.
    
    We suspect either the host cap is not implemented or there
    may be firmware specific issues. Firmware version is
    QC_IMAGE_VERSION_STRING=WLAN.HL.2.0.c3-00257-QCAHLSWMTPLZ-1
    
    qcom,snoc-host-cap-8bit-quirk didn't help. If I use this
    quirk, then the host capability request does get accepted,
    but we run into fatal "msa info req rejected" error and
    WiFi interface doesn't come up.
    
    Attempts are being made to debug the failure reasons but no
    luck so far. Hence this device specific workaround instead
    of checking for QMI_ERR_MALFORMED_MSG_V01 error message.
    Tried ath10k/WCN3990/hw1.0/wlanmdsp.mbn from the upstream
    linux-firmware project but it didn't help and neither did
    building board-2.bin file from stock bdwlan* files.
    
    This workaround will be removed once we have a viable fix.
    Thanks to postmarketOS guys for catching this.
    
    Signed-off-by: Amit Pundir <amit.pundir@linaro.org>

--- drivers/net/wireless/ath/ath10k/qmi.c
+++ drivers/net/wireless/ath/ath10k/qmi.c
@@ -653,7 +653,9 @@ static int ath10k_qmi_host_cap_send_sync(struct ath10k_qmi *qmi)
 
 	/* older FW didn't support this request, which is not fatal */
 	if (resp.resp.result != QMI_RESULT_SUCCESS_V01 &&
-	    resp.resp.error != QMI_ERR_NOT_SUPPORTED_V01) {
+	    resp.resp.error != QMI_ERR_NOT_SUPPORTED_V01 &&
+	    /* Xiaomi Poco F1 workaround */
+	    !of_machine_is_compatible("xiaomi,beryllium")) {
 		ath10k_err(ar, "host capability request rejected: %d\n", resp.resp.error);
 		ret = -EINVAL;
 		goto out;
