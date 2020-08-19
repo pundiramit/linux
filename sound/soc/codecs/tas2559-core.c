07ff68e78456c337b63e4afcc0a1809ff3188801
Amit Pundir <amit.pundir@linaro.org>
2020-08-19 09:48:36 +0530

    ASoC: tas2559: Update calibration file path for AOSP
    Signed-off-by: Amit Pundir <amit.pundir@linaro.org>

--- sound/soc/codecs/tas2559-core.c
+++ sound/soc/codecs/tas2559-core.c
@@ -40,7 +40,7 @@
 #include "tas2560.h"
 #include "tas2559-core.h"
 
-#define TAS2559_CAL_NAME    "/mnt/vendor/persist/audio/tas2559_cal.bin"
+#define TAS2559_CAL_NAME    "/vendor/firmware/qcom/sdm845/tas2559_cal.bin"
 #define RESTART_MAX 3
 
 static int tas2559_load_calibration(struct tas2559_priv *pTAS2559,
